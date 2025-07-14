19e0aecc8c9b0fbc4beba258a7cec8bbfde634b4
Mobin Tayebi <mbn.ty83@gmail.com>
2025-07-14 11:49:39 +0330

    feat: Implement basic website layout with menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
 // import { useState } from "react";
-import "./App.css";
 
-function App() {}
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      {/* menu of the website */}
+      <div className="bg-component">
+        <menu className="flex flex-row w-fit m-auto gap-10 mt-8 text-2xl font-serif p-4 rounded-4xl bg-[#45403e] text-[#082a2a]">
+          <a
+            href="#"
+            className=" hover:text-[#0b3b3b] hover:scale-110 transition-all duration-400 ease-out"
+          >
+            home
+          </a>
+          <a
+            href="#"
+            className=" hover:text-[#0b3b3b] hover:scale-110 transition-all duration-400 ease-out"
+          >
+            about us
+          </a>
+          <a
+            href="#"
+            className=" hover:text-[#0b3b3b] hover:scale-110 transition-all duration-400 ease-out"
+          >
+            company members
+          </a>
+          <a
+            href="#"
+            className=" hover:text-[#0b3b3b] hover:scale-110 transition-all duration-400 ease-out"
+          >
+            products
+          </a>
+        </menu>
+      </div>
+    </>
+  );
+}
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from './App.tsx'
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {
-      <div className="text-red-500">hello world</div>
-      /* <App /> */
-    }
+    <App />
   </StrictMode>
 );
